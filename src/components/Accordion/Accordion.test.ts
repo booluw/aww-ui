e0aa7b1fd1b574daaae482671fc3f70c4e367a5b
Boluwatife Johnson <jaycewap@gmail.com>
2026-03-08 23:07:56 +0100

    feat: add Breadcrumb component with variants and tests

--- src/components/Accordion/Accordion.test.ts
+++ src/components/Accordion/Accordion.test.ts
@@ -18,37 +18,4 @@ describe('Accordion', () => {
 
     expect(wrapper.element.tagName).toBe('DIV')
   })
-
-  // it('renders html in the accordion body', async () => {
-  //   const items = [
-  //     {
-  //       key: 'first',
-  //       title: 'Title of first item',
-  //       content: 'Content of first item',
-  //       isHtml: false,
-  //     },
-  //     {
-  //       key: 'first',
-  //       title: 'Title of first item',
-  //       content: '<b>Content of first item</b>',
-  //       isHtml: true,
-  //     }
-  //   ]
-    
-  //   const wrapper = mount(Accordion, {
-  //     props: { position: 'left', size: 'lg', items }
-  //   })
-
-  //   items.forEach(async (item) => {
-  //     const block = wrapper.find(`#${item.key}-html`)
-
-  //     if (item.isHtml) {
-  //       expect(block.exists()).toBeFalsy()
-  //       expect(block.html()).toContain(item.content.replace(/<\/?p.*?>/g, ''))
-  //     } else {
-  //       // expect(block.exists()).toBeTruthy()
-  //       // expect(block.html()).toContain(item.content)
-  //     }
-  //   })
-  // })
 })
